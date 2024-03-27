2c5c51aa4359f94b292acea6adda7afd26ab0c97
Matthew <f1driver05@gmail.com>
2024-03-27 14:10:50 +0000

    first commit

---
+++ src/main/java/space/faintlocket/villageroverhaul/VillagerOverhaul.java
@@ -0,0 +1,10 @@
+package space.faintlocket.villageroverhaul;
+
+import net.fabricmc.api.ModInitializer;
+
+public class VillagerOverhaul implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/space/faintlocket/villageroverhaul/client/VillagerOverhaulClient.java
@@ -0,0 +1,10 @@
+package space.faintlocket.villageroverhaul.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VillagerOverhaulClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
